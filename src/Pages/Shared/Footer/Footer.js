d4cbc849422c34b4787034b5df738b6b0e7e7462
sazalislam04 <mdsazalislam04@gmail.com>
2022-11-12 04:04:24 +0600

    added footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,61 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import whitening from "../../../assets/images/whitening.png";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>footer</h1>
-    </div>
+    <footer className="footer lg:grid-cols-4 p-10">
+      <div>
+        <img className="w-14 h-14" src={whitening} alt="" />
+        <p>
+          The Doctors Portal
+          <br />
+          Providing reliable tech since 1992
+        </p>
+      </div>
+      <div>
+        <span className="footer-title">Services</span>
+        <Link to="/" className="link link-hover">
+          Branding
+        </Link>
+        <Link to="/" className="link link-hover">
+          Design
+        </Link>
+        <Link to="/" className="link link-hover">
+          Marketing
+        </Link>
+        <Link to="/" className="link link-hover">
+          Advertisement
+        </Link>
+      </div>
+      <div>
+        <span className="footer-title">Company</span>
+        <Link to="/" className="link link-hover">
+          About us
+        </Link>
+        <Link to="/" className="link link-hover">
+          Contact
+        </Link>
+        <Link to="/" className="link link-hover">
+          Jobs
+        </Link>
+        <Link to="/" className="link link-hover">
+          Press kit
+        </Link>
+      </div>
+      <div>
+        <span className="footer-title">Legal</span>
+        <Link to="/" className="link link-hover">
+          Terms of use
+        </Link>
+        <Link to="/" className="link link-hover">
+          Privacy policy
+        </Link>
+        <Link to="/" className="link link-hover">
+          Cookie policy
+        </Link>
+      </div>
+    </footer>
   );
 };
 
